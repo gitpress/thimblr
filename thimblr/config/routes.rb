28f844f028d523e7b79e0492118badfcb4104204
GrilloPress <grillopress@gmail.com>
2014-11-18 23:04:55 +0000

    add imgpost and textpost resources

--- thimblr/config/routes.rb
+++ thimblr/config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  resources :image_posts
+
+  resources :text_posts
+
   resources :posts
 
   resources :users
--- thimblr/db/schema.rb
+++ thimblr/db/schema.rb
@@ -11,7 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141118223559) do
+ActiveRecord::Schema.define(version: 20141118225824) do
+
+  create_table "posts", force: true do |t|
+    t.string   "title"
+    t.text     "body"
+    t.string   "url"
+    t.string   "type"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "posts", ["user_id"], name: "index_posts_on_user_id"
 
   create_table "subscriptions", force: true do |t|
     t.integer  "leader_id"
